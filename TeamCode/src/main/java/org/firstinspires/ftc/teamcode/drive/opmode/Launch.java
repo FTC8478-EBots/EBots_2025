057767807ddc10ddcabe73006d4e6628977a0ab4
ebots8478 <146142210+ebots8478@users.noreply.github.com>
2025-10-15 19:22:00 -0400

    Add Launch telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Launch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Launch.java
@@ -1,23 +1,30 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-
+@Config
 public class Launch {
+   public static int LAUNCH_VELOCITY = 51200;
     DcMotorEx launchMotor;
     Gamepad gamepad;
+  Telemetry telemetry;
 
-
-    Launch(HardwareMap hardwareMap, Gamepad gamepad) {
+    Launch(HardwareMap hardwareMap, Gamepad gamepad, Telemetry telemetry) {
         launchMotor = hardwareMap.get(DcMotorEx.class, "launch");
         this.gamepad = gamepad;
         launchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         launchMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        this.telemetry = telemetry;
 
     }
 
@@ -25,8 +32,10 @@ public class Launch {
         if (gamepad.square) {
             launchMotor.setVelocity(0);
         }else if (gamepad.triangle) {
-            launchMotor.setVelocity(51200);
+            launchMotor.setVelocity(LAUNCH_VELOCITY);
         }
+        telemetry.addData("LAUNCH_VELOCITY:", launchMotor.getVelocity());
+
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Testteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Testteleop.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
-import com.acmerobotics.roadrunner.PoseVelocity2d;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 /**
  * This is a simple teleop routine for testing localization. Drive the robot around like a normal
@@ -20,29 +19,33 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 public class Testteleop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumDrive drive = new MecanumDrive(hardwareMap,null);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 Intake intake = new Intake(hardwareMap,gamepad2);
 Launch launch = new Launch(hardwareMap,gamepad2);
-
+ArtifactDetector artifactDetector = new ArtifactDetector(hardwareMap,gamepad2,telemetry);
+Hopper hopper = new Hopper(hardwareMap,gamepad2);
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         while (!isStopRequested()) {
-            drive.setDrivePowers(
-            new PoseVelocity2d( new Vector2d(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x),
-                    -gamepad1.right_stick_x
-            )
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
             );
 intake.processGamepad();
 launch.processGamepad();
-            Pose2d poseEstimate =             drive.localizer.getPose();
+artifactDetector.recognizeColor();
+hopper.processGamepad();
+            drive.update();
 
-
-            telemetry.addData("x", poseEstimate.position.x);
-            telemetry.addData("y", poseEstimate.position.y);
-            telemetry.addData("heading", poseEstimate.heading);
+            Pose2d poseEstimate = drive.getPoseEstimate();
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
             telemetry.update();
         }
     }
